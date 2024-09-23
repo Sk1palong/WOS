7951340a08d2575e5c881cfbab7359e50f8dfe5a
Sk1palong <cchs33@gmail.com>
2024-09-23 20:49:24 +0900

    init

---
+++ src/main/java/me/whitebear/wos/WosApplication.java
@@ -0,0 +1,13 @@
+package me.whitebear.wos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WosApplication.class, args);
+    }
+
+}
