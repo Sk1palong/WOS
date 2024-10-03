235badb1dea19bd94407dc601f11ef913514f9b5
Sk1palong <cchs33@gmail.com>
2024-10-03 17:24:19 +0900

    Hero 엔티티 수정 기능 추가

--- src/main/java/me/whitebear/wos/hero/HeroController.java
+++ src/main/java/me/whitebear/wos/hero/HeroController.java
@@ -2,12 +2,15 @@ package me.whitebear.wos.hero;
 
 import lombok.RequiredArgsConstructor;
 import me.whitebear.wos.hero.dto.request.HeroSaveReq;
+import me.whitebear.wos.hero.dto.request.HeroUpdateReq;
 import me.whitebear.wos.hero.dto.response.HeroGetRes;
 import me.whitebear.wos.hero.dto.response.HeroSaveRes;
+import me.whitebear.wos.hero.dto.response.HeroUpdateRes;
 import me.whitebear.wos.hero.service.HeroService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +33,13 @@ public class HeroController {
     @GetMapping("/{id}")
     public HeroGetRes getHero(@PathVariable Long id) {
         HeroGetRes res = heroService.getHero(id);
+
+        return res;
+    }
+
+    @PutMapping("/{id}")
+    public HeroUpdateRes updateHero(@PathVariable Long id, @RequestBody HeroUpdateReq req) {
+        HeroUpdateRes res = heroService.updateHero(id, req);
 
         return res;
     }
--- src/main/java/me/whitebear/wos/hero/service/HeroService.java
+++ src/main/java/me/whitebear/wos/hero/service/HeroService.java
@@ -3,10 +3,13 @@ package me.whitebear.wos.hero.service;
 import lombok.RequiredArgsConstructor;
 import me.whitebear.wos.hero.Hero;
 import me.whitebear.wos.hero.dto.request.HeroSaveReq;
+import me.whitebear.wos.hero.dto.request.HeroUpdateReq;
 import me.whitebear.wos.hero.dto.response.HeroGetRes;
 import me.whitebear.wos.hero.dto.response.HeroSaveRes;
+import me.whitebear.wos.hero.dto.response.HeroUpdateRes;
 import me.whitebear.wos.hero.repository.HeroRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -42,6 +45,17 @@ public class HeroService {
             .name(hero.getName())
             .type(hero.getType())
             .build();
+
+        return res;
+    }
+
+    @Transactional
+    public HeroUpdateRes updateHero(Long id, HeroUpdateReq req) {
+        Hero hero = heroRepository.findById(id).orElseThrow(IllegalArgumentException::new);
+
+        hero.updateHero(req.getName(), req.getGen(), req.getType());
+
+        HeroUpdateRes res = new HeroUpdateRes(hero);
 
         return res;
     }
