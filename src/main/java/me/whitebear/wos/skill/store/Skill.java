70dce683012905e1ba8ebcf7a8a502829e09fb97
Sk1palong <cchs33@gmail.com>
2024-10-10 20:20:51 +0900

    skill 엔티티 추가

---
+++ src/main/java/me/whitebear/wos/skill/store/Skill.java
@@ -0,0 +1,60 @@
+package me.whitebear.wos.skill.store;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "skill")
+public class Skill {
+
+    /**
+     * 컬럼 - 연관관계 컬럼을 제외한 컬럼을 정의합니다.
+     */
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    long id;
+
+    String name;
+
+    String description;
+
+    Integer level;
+
+    Integer ppl;
+
+    Integer percentage;
+
+    /**
+     * 생성자 - 약속된 형태로만 생성가능하도록 합니다.
+     */
+
+    @Builder
+    public Skill(String name, String description, Integer level, Integer ppl) {
+        this.name = name;
+        this.description = description;
+        this.level = level;
+        this.ppl = ppl;
+        this.percentage = level * ppl;
+    }
+
+    /**
+     * 연관관계 - Foreign Key 값을 따로 컬럼으로 정의하지 않고 연관 관계로 정의합니다.
+     */
+
+    /**
+     * 연관관계 편의 메소드 - 반대쪽에는 연관관계 편의 메소드가 없도록 주의합니다.
+     */
+
+    /**
+     * 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
+     */
+}
