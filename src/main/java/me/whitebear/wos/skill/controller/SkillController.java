b0b4fcce186b603102351e9e6bea9acd9ff038a1
Sk1palong <cchs33@gmail.com>
2024-10-16 18:10:33 +0900

    Skill 저장 기능 추가

--- src/main/java/me/whitebear/wos/skill/controller/SkillController.java
+++ src/main/java/me/whitebear/wos/skill/controller/SkillController.java
@@ -1,7 +1,11 @@
 package me.whitebear.wos.skill.controller;
 
 import lombok.RequiredArgsConstructor;
+import me.whitebear.wos.skill.dto.request.SkillSaveReq;
+import me.whitebear.wos.skill.dto.response.SkillSaveRes;
 import me.whitebear.wos.skill.service.SkillService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class SkillController {
 
     private final SkillService skillService;
+
+    @PostMapping
+    public SkillSaveRes saveSkill(@RequestBody SkillSaveReq req) {
+        SkillSaveRes res = skillService.saveSkill(req);
+
+        return res;
+    }
 }
--- src/main/java/me/whitebear/wos/skill/service/SkillService.java
+++ src/main/java/me/whitebear/wos/skill/service/SkillService.java
@@ -1,6 +1,9 @@
 package me.whitebear.wos.skill.service;
 
 import lombok.RequiredArgsConstructor;
+import me.whitebear.wos.skill.dto.request.SkillSaveReq;
+import me.whitebear.wos.skill.dto.response.SkillSaveRes;
+import me.whitebear.wos.skill.store.Skill;
 import me.whitebear.wos.skill.store.SkillRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,25 @@ import org.springframework.stereotype.Service;
 public class SkillService {
 
     private final SkillRepository skillRepository;
+
+    public SkillSaveRes saveSkill(SkillSaveReq req) {
+        Skill skill = Skill.builder()
+            .name(req.getName())
+            .type(req.getType())
+            .description(req.getDescription())
+            .value(req.getValue())
+            .build();
+
+        skillRepository.save(skill);
+
+        SkillSaveRes res = SkillSaveRes.builder()
+            .id(skill.getId())
+            .name(skill.getName())
+            .type(skill.getType())
+            .description(skill.getDescription())
+            .value(skill.getValue())
+            .build();
+
+        return res;
+    }
 }
