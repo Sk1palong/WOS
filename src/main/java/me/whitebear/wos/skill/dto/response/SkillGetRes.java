fcf68ba1fd77bb4010303a8711e065accabbe9d6
Sk1palong <cchs33@gmail.com>
2024-10-17 17:37:38 +0900

    Skill 단건조회 dto 추가

---
+++ src/main/java/me/whitebear/wos/skill/dto/response/SkillGetRes.java
@@ -0,0 +1,23 @@
+package me.whitebear.wos.skill.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+public class SkillGetRes {
+
+    Long id;
+    String name;
+    String type;
+    String description;
+    String value;
+
+    @Builder
+    public SkillGetRes(Long id, String name, String type, String description, String value) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+        this.description = description;
+        this.value = value;
+    }
+}
