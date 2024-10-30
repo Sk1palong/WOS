47913a7d62cce8d643ce0361f044dedc642f454d
Sk1palong <cchs33@gmail.com>
2024-10-30 17:55:12 +0900

    Widget 저장 ResponseDto 추가

---
+++ src/main/java/me/whitebear/wos/domain/widget/dto/response/WidgetSaveRes.java
@@ -0,0 +1,20 @@
+package me.whitebear.wos.domain.widget.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+import me.whitebear.wos.domain.hero.store.entity.Hero;
+import me.whitebear.wos.domain.widget.store.entity.Widget;
+
+@Getter
+public class WidgetSaveRes {
+    Long id;
+    String name;
+    String heroName;
+
+    @Builder
+    public WidgetSaveRes(Widget widget, Hero hero) {
+        this.id = widget.getId();
+        this.name = widget.getName();
+        this.heroName = hero.getName();
+    }
+}
