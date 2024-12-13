12de6ac0d65782f4f7789ae079d1c18fdc3ca141
Sk1palong <cchs33@gmail.com>
2024-12-13 16:46:19 +0900

    refactor

--- src/main/java/me/whitebear/wos/domain/hero/dto/response/HeroSaveRes.java
+++ src/main/java/me/whitebear/wos/domain/hero/dto/response/HeroSaveRes.java
@@ -7,15 +7,15 @@ import lombok.Getter;
 public class HeroSaveRes {
 
     Long heroId;
-    String name;
+    String heroName;
     Integer gen;
     String type;
     String grade;
 
     @Builder
-    public HeroSaveRes(Long heroId, String name, Integer gen, String type, String grade) {
+    public HeroSaveRes(Long heroId, String heroName, Integer gen, String type, String grade) {
         this.heroId = heroId;
-        this.name = name;
+        this.heroName = heroName;
         this.gen = gen;
         this.type = type;
         this.grade = grade;
