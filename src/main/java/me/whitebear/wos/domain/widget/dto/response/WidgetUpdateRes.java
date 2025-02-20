708cae4dcb74d4f2088e2321ca2e026970377f4f
Sk1palong <cchs33@gmail.com>
2025-02-20 16:06:10 +0900

    -refactor

--- src/main/java/me/whitebear/wos/domain/widget/dto/response/WidgetUpdateRes.java
+++ src/main/java/me/whitebear/wos/domain/widget/dto/response/WidgetUpdateRes.java
@@ -6,10 +6,13 @@ import me.whitebear.wos.domain.widget.store.entity.Widget;
 
 @Getter
 public class WidgetUpdateRes {
+
+    Long widgetId;
     String widgetName;
 
     @Builder
     public WidgetUpdateRes(Widget widget) {
+        this.widgetId = widget.getWidgetId();
         this.widgetName = widget.getWidgetName();
     }
 }
