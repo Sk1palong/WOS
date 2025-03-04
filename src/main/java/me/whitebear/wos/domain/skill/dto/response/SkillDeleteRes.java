6f3f3687b56544f286a8c4c6ded01607d6149659
Sk1palong <cchs33@gmail.com>
2025-03-04 16:16:07 +0900

    -refactor

--- src/main/java/me/whitebear/wos/domain/skill/dto/response/SkillDeleteRes.java
+++ src/main/java/me/whitebear/wos/domain/skill/dto/response/SkillDeleteRes.java
@@ -6,12 +6,15 @@ import me.whitebear.wos.domain.skill.store.entity.Skill;
 
 @Getter
 public class SkillDeleteRes {
+
     Long skillId;
     String skillName;
 
     @Builder
     public SkillDeleteRes(Skill skill) {
+
         this.skillId = skill.getSkillId();
         this.skillName = skill.getSkillName();
+
     }
 }
