a31a72824ea9552d43a5d434da7e33a924080430
Sk1palong <cchs33@gmail.com>
2025-03-13 17:55:22 +0900

    -refactor

--- src/main/java/me/whitebear/wos/domain/widget/store/entity/Widget.java
+++ src/main/java/me/whitebear/wos/domain/widget/store/entity/Widget.java
@@ -59,7 +59,9 @@ public class Widget extends Timestamped {
      * 서비스 메소드 - 외부에서 엔티티를 수정할 메소드를 정의합니다. (단일 책임을 가지도록 주의합니다.)
      */
     public void updateWidget(WidgetUpdateReq req) {
+
         this.widgetName = req.getWidgetName();
+
     }
 
 }
