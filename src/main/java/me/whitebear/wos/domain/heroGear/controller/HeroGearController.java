c86f5918fcd7a423161307d8358c717b594f20ec
Sk1palong <cchs33@gmail.com>
2025-04-29 23:15:57 +0900

    -saveHeroGears init

--- src/main/java/me/whitebear/wos/domain/heroGear/controller/HeroGearController.java
+++ src/main/java/me/whitebear/wos/domain/heroGear/controller/HeroGearController.java
@@ -1,6 +1,7 @@
 package me.whitebear.wos.domain.heroGear.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,5 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/wos/herogears")
 public class HeroGearController {
+
+    @PostMapping
+    public String saveHeroGears() {
+        return "saved hero gears";
+    }
+
 
 }
