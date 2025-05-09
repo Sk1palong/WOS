5f2ff3e8d2a6867f88bd925db3f6a00d746fa7ea
Sk1palong <cchs33@gmail.com>
2025-05-09 18:57:49 +0900

    -HeroGearSaveReq

---
+++ src/main/java/me/whitebear/wos/domain/heroGear/dto/request/HeroGearSaveReq.java
@@ -0,0 +1,12 @@
+package me.whitebear.wos.domain.heroGear.dto.request;
+
+import lombok.Getter;
+
+@Getter
+public class HeroGearSaveReq {
+
+    String heroGearName;
+    String type;
+    String grade;
+
+}
